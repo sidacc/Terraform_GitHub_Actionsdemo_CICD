982d70e6eaf373b17164d6e2ebb2edd8b739e7f6
sidacc <siddique.shaik@accenture.com>
2026-03-03 15:36:05 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "neeha-s3bucket-date"
+    bucket         = "skaraz-s3bucket-date"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "neeha-date-dblocks"
+    dynamodb_table = "skaraz-date-dblocks"
     encrypt        = true
   }
 }
